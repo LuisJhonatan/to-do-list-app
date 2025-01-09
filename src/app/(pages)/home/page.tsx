48cbfb84b26fbcf01b1fb0d1ee81df97f30b6988
LuisJhonatan <jhonatan.baca.5@gmail.com>
2025-01-08 20:58:18 -0500

    update

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,9 +1,7 @@
 "use client";
-import { useUser } from "@/app/context/UserContext";
 import Header from "@/components/Header";
 
 export default function Home (){
-    const user = useUser();
     
     return (
         <div>
